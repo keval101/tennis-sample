d360c183f1b0f0b6c719dafb3ccd880dfc906780
keval.vadhiya <kevalvadhiya101@gmail.com>
2023-01-30 14:15:52 +0530

    update enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/tennis/api2',
-  footballApiUrl: '/football/api/v1'
+  apiUrl: 'http://35.246.97.110/tennis/api2',
+  footballApiUrl: 'http://35.246.97.110/football/api/v1',
 };
